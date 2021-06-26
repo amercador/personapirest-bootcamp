7183c4fc3ebf3d11e24cb4f70dba2c31096cf14e
amercador <amercador@hotmail.com>
2021-06-26 20:49:22 -0300

    Created project initial

---
+++ src/main/java/one/digitalinnovation/personapirest/PersonapirestApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.personapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonapirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapirestApplication.class, args);
+	}
+
+}
