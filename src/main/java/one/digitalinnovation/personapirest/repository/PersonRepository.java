e60e663875f427321f50a2760995230f88cc598b
amercador <amercador@hotmail.com>
2021-06-27 10:12:55 -0300

    Developed person create operation

---
+++ src/main/java/one/digitalinnovation/personapirest/repository/PersonRepository.java
@@ -0,0 +1,7 @@
+package one.digitalinnovation.personapirest.repository;
+
+import one.digitalinnovation.personapirest.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
